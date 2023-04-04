4163ef2d3ad603662b07f8c33df7b4e202fff85e
Ale Grimaldi <grimaldialejandro5@gmail.com>
2023-04-04 03:11:28 -0300

    se ajusto el css de productos y agregar

---
+++ header.js
@@ -0,0 +1,18 @@
+document.getElementById("header").innerHTML = `
+<section class="contenedor__izquierdo">
+    <div class="contenedor__logo">
+        <a href="./index.html"><img class="header__logo" src="./assets/imagenes/header/Logo completo.png" alt="Logotipo de Alura Geek"></a>
+    </div>
+
+    <div class="contenedor__busqueda">
+        <input class="header__inpbusqueda" type="search" name="busqueda" id="" placeholder="¿Que desea buscar?">
+        <input type="submit" class="header__lupa" value="">
+    </div>
+</section>
+
+<section class="contenedor__derecho">
+    <a href="../2- Pagina Login/login.html" class="login__a">Login</a>
+</section>
+`;
+
+
